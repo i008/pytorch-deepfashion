6af636062aeab53c5de420d5631b476a5f2a0550
i008 <kubacieslik@gmail.com>
2018-09-08 22:41:07 +0200

    cleanup

--- deepfashion.py
+++ deepfashion.py
@@ -12,24 +12,6 @@ def adaptive_max_pool(input, size):
     return AdaptiveMaxPool2d(size[0], size[1])(input)
 
 
-def roi_pooling(input, rois, size=ROI_POOL_SIZE, spatial_scale=1.0):
-    assert (rois.dim() == 2)
-    assert (rois.size(1) == 5)
-    output = []
-    rois = rois.data.float()
-    num_rois = rois.size(0)
-
-    rois[:, 1:].mul_(spatial_scale)
-    rois = rois.long()
-    for i in range(num_rois):
-        roi = rois[i]
-        im_idx = roi[0]
-        im = input.narrow(0, im_idx, 1)[..., roi[2]:(roi[4] + 1), roi[1]:(roi[3] + 1)]
-        output.append(adaptive_max_pool(im, size)[0])
-
-    return torch.cat(output, 0)
-
-
 def gated_roi_pooling(input, rois, gates, size=ROI_POOL_SIZE, spatial_scale=1.0):
     assert (rois.dim() == 2)
     assert (rois.size(1) == 5)
